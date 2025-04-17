02aef1bc1188b1ff611afc41c3a386e83e2845df
Emin <emnalmdr@gmail.com>
2025-04-17 22:33:39 +0300

    added data blocks
    Signed-off-by: Emin <emnalmdr@gmail.com>

--- mgmt-cluster/cluster.tf
+++ mgmt-cluster/cluster.tf
@@ -8,6 +8,9 @@ data "aws_eks_cluster_auth" "this" {
   name = module.eks.cluster_name
 }
 
+data "aws_partition" "current" {}
+data "aws_caller_identity" "current" {}
+
 locals {
   name = var.cluster_name
 
